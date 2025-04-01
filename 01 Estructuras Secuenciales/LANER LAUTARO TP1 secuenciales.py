f14a289189035698c241b95b92388bb22ff811a7
Lautaro <lautilaner@gmail.com>
2025-03-31 21:58:33 -0300

    se subio tp2 y tp1

---
+++ 01 Estructuras Secuenciales/LANER LAUTARO TP1 secuenciales.py
@@ -0,0 +1,68 @@
+import math
+#saque corchetes para probar ejercicios
+#ejericio 1
+print("hola mundo!")
+
+#ejercicio 2
+#nombre = input("ingrese su nombre: ")
+#print(f"hola {nombre}!")
+
+#ejericicio 3
+#nombre = input("ingrese su nombre: ")
+#apellido = input("ingrese su apellido: ")
+#edad = input("ingrese su edad: ")
+#residencia = input("ingrese su pais de residencia: ")
+#print (f"soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+#ejercicio 4
+#radio = float(input("ingrese el radio de un circulo en cm: "))
+#area = math.pi * radio ** 2
+#perimetro = 2 * math.pi * radio
+#print (f"el area del circulo es {area} y el perimetro es {perimetro}")
+
+#ejercicio 5
+#segundos = float(input("ingrese cuantos segundos quiere convertir a horas: "))
+#horas = float(segundos / 3600)
+#print (f"los segundos ingresados equivalen a {horas} horas")
+
+#ejercicio 6
+#numero = int(input("ingrese un numero entre 1 y 9: "))
+#print (numero)
+#print (numero * 2)
+#print (numero * 3)
+#print (numero * 4)
+#print (numero * 5)
+#print (numero * 6)
+#print (numero * 7)
+#print (numero * 8)
+#print (numero * 9)
+#print (numero * 10)
+#considere usar for pero decidi hacerlo asi ya que el trabajo practico es de secuenciales
+
+#ejercicio 7
+#num1 = float(input("ingrese el primer numero: "))
+#num2 = float(input("ingrese el segundo numero: "))
+#print (f"el primer numero mas el segundo es {num1 + num2}")
+#print (f"el primer numero menos el segundo es {num1 - num2}")
+#print (f"el segundo numero menos el primeo es {num2 - num1}")
+#print (f"el primer numero por el segundo es {num1 * num2}")
+#print (f"el primer numero dividido el segundo es {num1 / num2}")
+#print (f"el primer numero divido el segundo es {num2 / num1}")
+
+#ejercicio 8
+#peso = float(input("ingrese su peso en kg: "))
+#altura = float(input("ingrese su altura en metros: "))
+#imc = peso / altura ** 2
+#print (f"su indice de masa corporal es {imc}")
+
+#ejercicio 9
+#celsius = float(input("ingrese la temperatura en celsius: "))
+#fahr = float((9/5) * celsius + 32)
+#print (f"la tempertura en celsius convertida a fahrenheit es {fahr}")
+
+#ejercicio 10
+#num1 = float(input("ingrese el primer numero: "))
+#num2 = float(input("ingrese el segundo numero: "))
+#num3 = float(input("ingrese el tercer numero: "))
+#promedio = float((num1 + num2 + num3) / 3)
+#print(f"el promedio de los 3 numeros es igual a {promedio}")
