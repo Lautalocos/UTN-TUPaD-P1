f10c337edc3624595e92e9a6ced8896096170e26
Lautaro <lautilaner@gmail.com>
2025-04-27 21:18:28 -0300

    se hace entrega de tp 5.1 de listas

---
+++ 05.1 Listas/tp 5 listas.py
@@ -0,0 +1,67 @@
+# ejercicio 1
+
+# lista_1_a_100= list(range(0, 100, 4))
+
+# print(lista_1_a_100)
+
+# ejercicio 2
+
+#lista = ["l" ,"a", "u","t", "i"]
+
+#print (lista[3])
+
+#ejercicio 3
+
+# lista = []
+
+# lista.append("nombre")
+# lista.append("apellido")
+# lista.append("edad")
+# print (lista)
+
+#ejercicio 4
+
+# animales = ["perro", "gato", "conejo", "pez"]
+# animales[1] = "loro"
+# animales[3] = "oso"
+
+# print(animales)
+
+#ejercicio 5
+# el programa compara los elementos de la lista,
+# retorna el mas alto y lo remueve
+
+#ejercicio 6
+# lista_10_30= list(range(10, 31, 5))
+
+# print(lista_10_30[0])
+# print(lista_10_30[1])
+
+#ejercicio 7
+# autos = ["sedan", "polo", "suran", "gol"]
+# autos[1] = ("jeep")
+# autos[2] = ("renegade")
+
+# print (autos)
+
+#ejercicio 8
+# dobles = []
+# dobles.append(5 * 2)
+# dobles.append(10 * 2)
+# dobles.append(15 * 2)
+
+# print(dobles)
+
+#ejercicio 9
+# compras = [["pan", "leche"], ["arroz", "fideos", "salsa"],["agua"]]
+
+# compras[2].append("jugo")
+# compras[1][1] = "tallarines"
+# compras[0].remove("pan")
+
+# print (compras)
+
+#ejercicio 10
+#lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+
+#print(lista_anidada)
